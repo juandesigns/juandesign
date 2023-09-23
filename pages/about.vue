9cf780ab1e9e727cb87942ff812773ef949e6b08
Jason <ya850805@gmail.com>
2023-09-24 00:10:57 +0800

    feat: Init commit.

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is about page</h1>
+    <NuxtLink to="/">To home page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is Home page.</h1>
+    <NuxtLink to="/about">To about page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
