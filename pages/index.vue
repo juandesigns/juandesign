6401ab3f4c558404b8b474617369142d8036b8fd
SHIOU-JIUAN JUANG <rsa0958611612@gmail.com>
2023-09-27 01:40:47 +0800

    fix:dark btn Add:favicon icon

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
       <section class="banner-block flex justify-center">
         <div class="container">
           <div>
-            <h1>打造你的專屬網站<br/>準時交件 <br class="mobile"/>價格優惠 <br class="mobile"/>用心負責</h1>
+
+            <h1>打造你的專屬網站<br/><span class="h2">準時交件 <br class="mobile"/>價格優惠 <br class="mobile"/>用心負責</span></h1>
 
             <p class="h5">
               我們夥伴包含<br class="mobile"/>UIUX設計師、全端開發工程師！<br/>
@@ -17,14 +18,14 @@
       </section>
       <section class="service-block flex justify-center">
         <div class="container">
-          <div class="flex flex-col items-center w-30">
+          <div class="flex flex-col items-center w-20">
             <div class="flex justify-center relative">
               <img width="120" src="../assets/img/img-title-service.png">
               <span class="h2 title-tag whitespace-nowrap">服務項目</span>
             </div>
             <p class="en-txt">SERVICE</p>
           </div>
-          <div class="flex flex-wrap justify-end w-70">
+          <div class="flex flex-wrap justify-end w-80">
             <div class="img-content">
               <img src="../assets/img/img-service-01.png">
               <p class="h3">網站建置</p>
@@ -39,7 +40,7 @@
               </div>
             </div>
             <div class="img-content">
-              <img src="../assets/img/img-service-01.png">
+              <img src="../assets/img/img-service-02.png">
               <p class="h3">網站架設與網域</p>
               <p>客製化網域</p>
               <p>虛擬主機架設</p>
@@ -52,7 +53,7 @@
               </div>
             </div>
             <div class="img-content">
-              <img src="../assets/img/img-service-01.png">
+              <img src="../assets/img/img-service-03.png">
               <p class="h3">軟體開發</p>
               <p>前端/後端開發</p>
               <p>爬蟲程式服務</p>
@@ -65,7 +66,7 @@
               </div>
             </div>
             <div class="img-content">
-              <img src="../assets/img/img-service-01.png">
+              <img src="../assets/img/img-service-04.png">
               <p class="h3">UIUX設計及切版</p>
               <p>UI/UX設計</p>
               <p>網頁切版服務</p>
@@ -78,7 +79,7 @@
               </div>
             </div>
             <div class="img-content">
-              <img src="../assets/img/img-service-01.png">
+              <img src="../assets/img/img-service-05.png">
               <p class="h3">品牌經營與推廣</p>
               <p>LINE@BOT 建立</p>
               <p>FB messenger 交談功能</p>
