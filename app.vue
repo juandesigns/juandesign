66b87121a6e3a79f0641b8378292f006e64820ad
Jason <ya850805@gmail.com>
2023-10-25 17:09:32 +0800

    feat: Add nuxt3 facebook chat plugin

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@
     <NuxtLayout>
       <NuxtPage></NuxtPage>
     </NuxtLayout>
-
+    <client-only>
+      <nuxt-facebook-chat :options="options" />
+    </client-only>
     <TheFooter/>
 
 <!--    <UButton>123</UButton>-->
@@ -13,6 +15,17 @@
 
   </div>
 </template>
+
+<script setup lang="ts">
+import { NuxtFacebookChatOptions } from 'nuxt-facebook-chat';
+// Define your chat options
+const options: NuxtFacebookChatOptions = {
+  pageId: "122487314278162",  // Your Facebook Page ID
+  locale: "zh_TW",            // Set the locale for the chat
+  themeColor: "#E04040",      // Customize the chat theme color
+};
+</script>
+
 <style>
 
 </style>
