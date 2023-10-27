9aec35d5c7c47e96d91f127da74a198c3a0fd267
Jason <ya850805@gmail.com>
2023-10-27 11:36:24 +0800

    feat: Add contact form validation

--- pages/contact.vue
+++ pages/contact.vue
@@ -30,7 +30,8 @@
         <div class="flex flex-col grid gap-y-6 max-w-md">
           <h1>聯絡表單</h1>
           <UForm
-              :validate="validate"
+              ref="form"
+              :schema="schema"
               :state="state"
               @submit="submit"
               class="grid gap-y-6"
@@ -69,7 +70,19 @@
 
 <script setup lang="ts">
 import {ref} from 'vue'
+import { object, string, InferType } from 'yup'
 import type {FormError, FormSubmitEvent} from '@nuxt/ui/dist/runtime/types'
+
+const form = ref()
+
+const schema = object({
+  name: string().max(30, '姓名不得大於30字').required('請輸入聯絡人姓名'),
+  email: string().email('email格式錯誤').required('請輸入聯絡信箱'),
+  title: string().max(50, '主旨不得大於50字').required('請輸入主旨'),
+  content: string().max(300, '訊息不得大於300字').required('請輸入訊息')
+})
+
+type Schema = InferType<typeof schema>
 
 const state = ref({
   name: undefined,
@@ -78,17 +91,8 @@ const state = ref({
   content: undefined,
 })
 
-//TODO other validation
-const validate = (state: any): FormError[] => {
-  const errors = []
-  if (!state.name) errors.push({path: 'name', message: '請輸入聯絡人姓名'})
-  if (!state.email) errors.push({path: 'email', message: '請輸入聯絡信箱'})
-  if (!state.title) errors.push({path: 'title', message: '請輸入主旨'})
-  if (!state.content) errors.push({path: 'content', message: '請輸入訊息'})
-  return errors
-}
 
-async function submit(event: FormSubmitEvent<any>) {
+async function submit(event: FormSubmitEvent<Schema>) {
   const message = `
   收到信息了！
   姓名：${event.data.name}
@@ -101,10 +105,20 @@ async function submit(event: FormSubmitEvent<any>) {
     message: message
   }
 
-  await fetch('https://script.google.com/macros/s/AKfycby-p3pkm46nnXAdHPESo1I8J6CgiyVDUpeRfKGc_q8L04ArtQScNclMoE3ciSGn6rcm/exec', {
+  fetch('https://script.google.com/macros/s/AKfycby-p3pkm46nnXAdHPESo1I8J6CgiyVDUpeRfKGc_q8L04ArtQScNclMoE3ciSGn6rcm/exec', {
     body: JSON.stringify(data),
     method: 'POST',
     mode: 'no-cors'
+  }).then(res => {
+    window.alert('發送表單成功！')
+    form.value.clear() //clear errors
+    event.data.name = ''
+    event.data.email = ''
+    event.data.title = ''
+    event.data.content = ''
+  }).catch(err => {
+    window.alert('發送表單失敗...' + err)
+    console.log(err)
   })
 }
 
