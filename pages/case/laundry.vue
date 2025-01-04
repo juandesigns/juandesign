cd1a812753bd12d607cb90b72c7341c04482def3
SHIOU-JIUAN JUANG <rsa0958611612@gmail.com>
2025-01-04 17:58:54 +0800

    fix:new word and png

--- pages/case/laundry.vue
+++ pages/case/laundry.vue
@@ -3,10 +3,14 @@
     <TheBanner :title="'相關案例'" :description="'新光白洗衣店LINE@預約收送'"/>
     <section class="inner-block flex flex-col items-center">
       <div class="container">
-        <div class="flex flex-col grid gap-y-6">
+        <div class="flex-col grid gap-y-6 text-left lg:text-center">
           <h1>新光白洗衣店LINE@預約收送</h1>
-          <p>透過LINE@與Google sheet串接存取訂單，發送LINE Notify通知商家訂單，同時LINE@通知客人已收到預約訂單。</p>
-          <img src="../../assets/img/laundry-banner.png"/>
+          <p>整合LINE@與Google Sheet，打造智慧化訂單管理系統，讓客戶感受到即時回應與貼心服務。</p>
+          <p>當客戶下單後，系統自動將訂單資訊存入Google Sheet，確保資料即時更新與集中管理。</p>
+          <p>透過LINE Notify功能，商家能即時接收訂單通知，有效減少漏單或延遲處理的風險，</p>
+          <p>同時，系統自動向客戶發送LINE@通知，確認預約已成功登記。</p>
+          <img class="hidden md:block" alt="新光白洗衣店" src="../../assets/img/laundry-banner.png"/>
+          <img class="block md:hidden" alt="新光白洗衣店" src="../../assets/img/img-case-07.png"/>
         </div>
       </div>
     </section>
@@ -19,7 +23,9 @@
         </ul>
         <ul class="list-[square] pl-4 leading-8">
           <li>合作商家：新白光洗衣店</li>
-          <li>LINE@：<a target="_blank" href="https://liff.line.me/1645278921-kWRPP32q/?accountId=978nvgpx" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">立即加入LINE@</a></li>
+          <li>LINE@：<a target="_blank" href="https://liff.line.me/1645278921-kWRPP32q/?accountId=978nvgpx"
+                       class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">立即加入LINE@</a>
+          </li>
         </ul>
       </div>
     </section>
@@ -30,7 +36,9 @@
             <h3 class="font-bold mb-4">採用服務</h3>
             <li>品牌經營與推廣</li>
             <li>LINE @ 機器人服務</li>
-            <li>機器人互動方案 <a target="_blank" href="/service/brand" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a></li>
+            <li>機器人互動方案 <a target="_blank" href="/service/brand"
+                                  class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a>
+            </li>
           </ul>
           <ul class="list-[square] pl-4 leading-8">
             <h3 class="font-bold mb-4">相關技術</h3>
--- pages/case/shipping.vue
+++ pages/case/shipping.vue
@@ -3,34 +3,46 @@
     <TheBanner :title="'相關案例'" :description="'臺瑞集運官網建置'"/>
     <section class="inner-block flex flex-col items-center">
       <div class="container">
-        <div class="flex flex-col grid gap-y-6">
+        <div class="flex-col grid gap-y-6 text-left lg:text-center">
           <h1>臺瑞集運官網建置</h1>
-          <p>官網包含常見問題、客戶好評、品牌優勢等資訊傳遞，透過不同運送及品項，提供對應試算運費功能，降低人力負載，提高經營效率，採用AWS雲服務及專屬域名，同時展示品牌專業形象。</p>
-          <img src="../../assets/img/shipping-banner.png"/>
+          <p>針對網站提升品牌形象與使用者體驗，整合常見問題、客戶好評與品牌優勢等功能，快速提供資訊，增進信任感。</p>
+          <p>運費試算功能自動計算費用，減少人力成本，提升營運效率，為消費者帶來便捷。</p>
+          <p>網站架設於AWS雲端，使用專屬域名，確保穩定與安全，展現品牌專業價值，是品牌與顧客的重要橋樑。</p>
+          <img class="hidden md:block" alt="臺瑞集運" src="../../assets/img/shipping-banner.png"/>
+          <img class="block md:hidden" alt="臺瑞集運" src="../../assets/img/img-case-08.png"/>
         </div>
       </div>
     </section>
     <section class="flex justify-center bg-tag">
       <div class="container p-6 lg:p-8 grid grid-cols-1 lg:grid-cols-2 gap-y-6">
         <ul class="list-[square] pl-4 leading-8">
-          <li>時程：2023/10</li>
+          <li>時程：2024/10</li>
           <li>範圍：UI設計、前端頁面開發、試算功能、網站架設及自訂義網域</li>
           <li>狀態：已上線</li>
         </ul>
         <ul class="list-[square] pl-4 leading-8">
           <li>合作商家：臺瑞集運</li>
-          <li>臺瑞集運 | <a target="_blank" href="https://terryshipping.tw/" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">terry.shipping.tw</a></li>
+          <li>網站類型 | 一頁式網站</li>
+          <li>前往查看 | <a target="_blank" href="https://terryshipping.tw/"
+                            class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">terry.shipping.tw</a>
+          </li>
         </ul>
       </div>
     </section>
     <section class="inner-block flex justify-center">
       <div class="container">
-        <div class=" lg:pb-8 grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-8">
+        <div class=" lg:pb-8 grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-14">
           <ul class="list-[square] pl-4 leading-8">
             <h3 class="font-bold mb-4">採用服務</h3>
-            <li>UIUX設計、含RWD動態網頁切版 <a target="_blank" href="/service/uiux" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a></li>
-            <li>前端功能開發 <a target="_blank" href="/service/develop" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a></li>
-            <li>網站架設及自訂義網域 <a target="_blank" href="/service/websetup" class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a></li>
+            <li>UIUX設計、含RWD動態網頁切版 <a target="_blank" href="/service/uiux"
+                                              class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a>
+            </li>
+            <li>前端功能開發 <a target="_blank" href="/service/develop"
+                                class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a>
+            </li>
+            <li>網站架設及自訂義網域 <a target="_blank" href="/service/websetup"
+                                        class="border-b-2 border-red-300 text-red-400 pr-1 font-bold hover:text-[#B38F8AFF] hover:border-[#B38F8AFF] transition duration-300">前往了解</a>
+            </li>
           </ul>
           <ul class="list-[square] pl-4 leading-8">
             <h3 class="font-bold mb-4">相關技術</h3>
@@ -38,10 +50,18 @@
             <li>html、css、js、bootstrap</li>
             <li>AWS、Godaddy</li>
           </ul>
+          <ul class="list-[square] pl-4 leading-8">
+            <h3 class="font-bold mb-4">手機版</h3>
+            <img class="shadow" src="../../assets/img/shipping-img-1.png"/>
+          </ul>
+          <ul class="list-[square] pl-4 leading-8">
+            <h3 class="font-bold mb-4">電腦版</h3>
+            <img class="shadow" src="../../assets/img/shipping-img-2.png"/>
+          </ul>
         </div>
+
       </div>
     </section>
-
     <TheContact/>
   </div>
 </template>
